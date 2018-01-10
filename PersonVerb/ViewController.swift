6e71c5b8ea564957bcea6c55f3045b454423e9ef
GongHua Guo <jiahui@GongHuadeMacBook-Pro.local>
2018-01-10 15:10:35 +0800

    Initial Commit

---
+++ PersonVerb/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PersonVerb
+//
+//  Created by GongHua Guo on 2018/1/10.
+//  Copyright © 2018年 GongHua Guo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
